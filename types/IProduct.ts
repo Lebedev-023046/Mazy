5b9b77dec807b4757179fa4f74db5008cfd170ef
lebedev-023046 <lebedev.023046@gmail.com>
2023-01-31 14:40:45 +0300

    feat: add product interface

---
+++ types/IProduct.ts
@@ -0,0 +1,13 @@
+export interface IProduct {
+  id: string;
+  name: string;
+  size: number[];
+  color: string;
+  price: number;
+  img: string;
+  slug: string;
+  year: number;
+  popular: boolean;
+  brand: string;
+  quantity: number;
+}
