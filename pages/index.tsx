10e89e961195f1fbae1317743c900b9c84e43e65
lebedev-023046 <lebedev.023046@gmail.com>
2023-01-31 14:41:38 +0300

    feat: add goods render

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,27 @@
 import Layout from "@/components/Layout";
+import { ProductItem } from "@/components/ProductItem";
+import { data } from "@/utils/data";
+// import { IProduct } from "@/types";
 import React from "react";
 
 const Home: React.FC = function () {
+  // useEffect(() => {
+  //   try {
+  //     fetch("data.json")
+  //       .then((res) => res.json())
+  //       .then((data) => setGoods(data));
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }, []);
+
   return (
     <Layout title="Home Page">
-      <div>Home Page</div>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem key={product.id} product={product} />
+        ))}
+      </div>
     </Layout>
   );
 };
