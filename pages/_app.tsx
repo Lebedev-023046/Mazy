9844b14b531e7121c8403e87f6b87edea5a587ab
lebedev-023046 <lebedev.023046@gmail.com>
2023-02-07 10:20:47 +0300

    feat: add redux provider to a project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
+import { setupStore } from "@/store";
 import "@/styles/global.css";
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+
+const store = setupStore();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />;
+    </Provider>
+  );
 }
