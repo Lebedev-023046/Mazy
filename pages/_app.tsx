a0fbc395552636edcbd150816b030e6514a9f094
lebedev-023046 <lebedev.023046@gmail.com>
2023-03-06 00:51:09 +0300

    feat: add session provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,20 @@
 import { setupStore } from "@/store";
 import "@/styles/global.css";
 import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 import { Provider } from "react-redux";
 
 const store = setupStore();
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />;
-    </Provider>
+    <SessionProvider session={session}>
+      <Provider store={store}>
+        <Component {...pageProps} />;
+      </Provider>
+    </SessionProvider>
   );
 }
