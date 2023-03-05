39f1de3dde739e1b9a97ee0f8ac6d69c01042980
lebedev-023046 <lebedev.023046@gmail.com>
2023-03-06 00:57:23 +0300

    feat: add custom fields for Sessoion and User interfaces from nextAuth

---
+++ types/next-auth.d.ts
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-unused-vars
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  export interface Session {
+    id: string;
+    isAdmin: string;
+    user: {
+      _id: string;
+      name: string;
+      email: string;
+      password: string;
+      isAdmin: boolean;
+    };
+  }
+  export interface User {
+    _id: string;
+    email: string;
+    password: string;
+    isAdmin: boolean;
+  }
+}
