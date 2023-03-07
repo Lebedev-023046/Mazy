42d10c51738439d2a8ed5911c5a9784df72c006b
lebedev-023046 <lebedev.023046@gmail.com>
2023-03-08 00:46:10 +0300

    feat: add subheader for shipping page

---
+++ components/CheckoutWizard.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function CheckoutWizard({ activeSteps = 0 }) {
+  return (
+    <div className="mb-5 flex flex-wrap">
+      {["User Login", "Shipping Adderss", "Payment Order", "Place Order"].map(
+        (step, index) => (
+          <div
+            key={step}
+            className={`flex-1 border-b-2 text-center ${
+              index <= activeSteps
+                ? "border-indigo-500 text-indigo-500"
+                : "border-gray-400 text-gray-400"
+            }`}
+          >
+            {step}
+          </div>
+        )
+      )}
+    </div>
+  );
+}
