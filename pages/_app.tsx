fde5f3b8891c8cf3791933d75776d47c4bf5de7c
lebedev-023046 <lebedev.023046@gmail.com>
2023-03-08 00:45:08 +0300

    feat: add protected routes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,51 @@
+import React from "react";
 import { setupStore } from "@/store";
 import "@/styles/global.css";
 import type { AppProps } from "next/app";
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider, useSession } from "next-auth/react";
 import { Provider } from "react-redux";
+import { useRouter } from "next/router";
+import { NextComponentType } from "next/types";
+
+interface IAuthProps {
+  children: any;
+}
+
+type CustomAppProps = AppProps & {
+  Component: NextComponentType & { auth?: boolean };
+};
 
 const store = setupStore();
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
-}: AppProps) {
+}: CustomAppProps) {
   return (
     <SessionProvider session={session}>
       <Provider store={store}>
-        <Component {...pageProps} />;
+        {Component.auth ? (
+          <Auth>
+            <Component {...pageProps} />;
+          </Auth>
+        ) : (
+          <Component {...pageProps} />
+        )}
       </Provider>
     </SessionProvider>
   );
+}
+
+function Auth({ children }: IAuthProps) {
+  const router = useRouter();
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push("/unauthorized?message=login required");
+    },
+  });
+  if (status === "loading") {
+    return <div>Is loading...</div>;
+  }
+  return children;
 }
