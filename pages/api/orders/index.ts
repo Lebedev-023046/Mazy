a5139ef9bd0d47c05de1125d21a0556c45a0f338
lebedev-023046 <lebedev.023046@gmail.com>
2023-03-24 15:01:29 +0300

    feat: create handler for ordres

---
+++ pages/api/orders/index.ts
@@ -0,0 +1,21 @@
+import { getSession } from "next-auth/react";
+import Order from "../../../models/Order";
+import db from "../../../utils/db";
+
+const handler = async (req: any, res: any) => {
+  const session = await getSession({ req });
+  if (!session) {
+    return res.status(401).send("signin required");
+  }
+
+  const { user } = session;
+  await db.connect();
+  const newOrder = new Order({
+    ...req.body,
+    user: user._id,
+  });
+
+  const order = await newOrder.save();
+  res.status(201).send(order);
+};
+export default handler;
