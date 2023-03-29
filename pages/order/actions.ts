5c82b4772523f3f4f0f5f95021a9cb50e455dfab
lebedev-023046 <lebedev.023046@gmail.com>
2023-03-29 18:25:11 +0300

    feat: add actions for Order Screen local state

---
+++ pages/order/actions.ts
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+const enum Actions {
+  FETCH_REQUEST = "FETCH_REQUEST",
+  FETCH_SUCCESS = "FETCH_SUCCESS",
+  FETCH_FAIL = "FETCH_FAIL",
+}
+
+export default Actions;
