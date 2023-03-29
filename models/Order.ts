85786de437b846612ec5ba6648f0a550210936c5
lebedev-023046 <lebedev.023046@gmail.com>
2023-03-29 18:23:08 +0300

    feat: add slug field to order model

--- models/Order.ts
+++ models/Order.ts
@@ -1,10 +1,12 @@
+import { IOrder } from "@/types";
 import mongoose from "mongoose";
 
-const orderSchema = new mongoose.Schema(
+const orderSchema = new mongoose.Schema<IOrder>(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     orderItems: [
       {
+        slug: { type: String, required: true },
         name: { type: String, required: true },
         quantityInCart: { type: Number, required: true },
         image: { type: String, required: true },
